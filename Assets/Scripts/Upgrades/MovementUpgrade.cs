e06448d83e034a10bbd157826fe168ed4d72f6e9
Yannick Garbe <Yannickgarbe02@gmail.com>
2026-05-18 11:37:36 +0200

    add: upgrade script and logic

--- Assets/Scripts/Upgrades/MovementUpgrade.cs
+++ Assets/Scripts/Upgrades/MovementUpgrade.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class MovementUpgrade : MonoBehaviour
 {
+    public float DamageIncrease = 1.5f;
+    public float DrainRateIncrease = 1.5f;
+
     private void Start()
     {
-        
+        PlayerMotor motor = GetComponent<PlayerMotor>();
+        SanityManager manager = FindFirstObjectByType<SanityManager>();
+
+        motor.IncreaseMultiplier(DamageIncrease);
+        manager.IncreaseDrainAmount(DrainRateIncrease);
     }
 }
