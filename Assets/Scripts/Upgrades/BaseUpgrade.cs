14a8f340db5422ee4b0a5849da06e1edd350d543
Yannick Garbe <Yannickgarbe02@gmail.com>
2026-05-18 12:06:20 +0200

    feat: upgrade can be added to player

---
+++ Assets/Scripts/Upgrades/BaseUpgrade.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public abstract class BaseUpgrade : MonoBehaviour
+{
+    //empty class
+}
--- Assets/Scripts/Upgrades/MovementUpgrade.cs
+++ Assets/Scripts/Upgrades/MovementUpgrade.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 
-public class MovementUpgrade : MonoBehaviour
+public class MovementUpgrade : BaseUpgrade
 {
     public float DamageIncrease = 1.5f;
     public float DrainRateIncrease = 1.5f;
-
+    private PlayerMotor motor;
+    private SanityManager manager;
     private void Start()
     {
-        PlayerMotor motor = GetComponent<PlayerMotor>();
-        SanityManager manager = FindFirstObjectByType<SanityManager>();
+        motor = GetComponent<PlayerMotor>();
+        manager = FindFirstObjectByType<SanityManager>();
 
         motor.IncreaseMultiplier(DamageIncrease);
         manager.IncreaseDrainAmount(DrainRateIncrease);
+    }
+
+    private void OnDestroy()
+    {
+        motor.IncreaseMultiplier(1);
+        manager.IncreaseDrainAmount(1);
     }
 }
