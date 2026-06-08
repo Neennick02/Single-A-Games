05e98a357704db43ba5fb21dccbb33125ce096f4
Yannick Garbe <Yannickgarbe02@gmail.com>
2026-06-08 09:48:22 +0200

    fix: keep buddy on new scene load

--- Assets/Scripts/Upgrades/Buddy/BuddyUpgrade.cs
+++ Assets/Scripts/Upgrades/Buddy/BuddyUpgrade.cs
@@ -4,7 +4,22 @@ public class BuddyUpgrade : MonoBehaviour
 {
     public GameObject BuddyPrefab;
 
+    private void OnEnable()
+    {
+        SceneLoader.OnMainSceneLoad += SpawnBuddy;
+    }
+
+    private void OnDisable()
+    {
+        SceneLoader.OnMainSceneLoad -= SpawnBuddy;
+    }
+
     private void Start()
+    {
+        SpawnBuddy();
+    }
+
+    private void SpawnBuddy()
     {
         Vector3 pos = Camera.main.transform.position + new Vector3(0, 0, 2);
 
