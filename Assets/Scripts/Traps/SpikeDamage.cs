5222fbd631f85dec133471d4d1363528381876d1
Yannick Garbe <Yannickgarbe02@gmail.com>
2026-06-09 13:17:23 +0200

    feat: ad static spike traps

--- Assets/Scripts/Traps/SpikeDamage.cs
+++ Assets/Scripts/Traps/SpikeDamage.cs
@@ -1,22 +1,67 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpikeDamage : MonoBehaviour
 {
     public float DamageAmount = 1f;
+    public float ResetDur = 1f;
+    private float timer;
+
+    public List<GameObject> ObjectsInRange = new List<GameObject>();
+
+    private void Start()
+    {
+        timer = ResetDur;
+    }
+    private void Update()
+    {
+        if(ObjectsInRange.Count > 0)
+        {
+            timer += Time.deltaTime;
+
+            if(timer >= ResetDur)
+            {
+                DealDamage();
+                timer = 0;
+            }
+        }
+    }
+
+    private void DealDamage()
+    {
+        for(int i = 0; i < ObjectsInRange.Count; i++)
+        {
+            GameObject obj = ObjectsInRange[i];
+
+            PlayerHealth Phealth = obj.GetComponent<PlayerHealth>();
+            EnemyHealth  Ehealth = obj.GetComponent<EnemyHealth>();
+
+            if(Phealth != null)
+            {
+                Phealth.TakeDamage(DamageAmount);
+            }
+            else if(Ehealth != null)
+            {
+                Ehealth.TakeDamage(DamageAmount);
+            }
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") ||
+            other.gameObject.CompareTag("Enemy"))
         {
-            PlayerHealth health = other.gameObject.GetComponent<PlayerHealth>();
-
-            health.TakeDamage(DamageAmount);
+            ObjectsInRange.Add(other.gameObject);
+            DealDamage();
         }
-        else if (other.gameObject.CompareTag("Enemy"))
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (ObjectsInRange.Contains(other.gameObject))
         {
-
-            EnemyHealth health = other.gameObject.GetComponent<EnemyHealth>();
-
-            health.TakeDamage(DamageAmount);
+            ObjectsInRange.Remove(other.gameObject);
         }
     }
 }
