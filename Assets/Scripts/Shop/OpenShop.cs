b3ba7bec652c9bfcc3e15a758ad85d5d18f0d759
Nee_nnick <Yannickgarbe02@gmail.com>
2026-06-12 14:44:04 +0100

    add: shop model and image

--- Assets/Scripts/Shop/OpenShop.cs
+++ Assets/Scripts/Shop/OpenShop.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 public class OpenShop : MonoBehaviour
 {
     public static event Action OnOpenShop;
+    private Collider collider;
+
+    private void Start()
+    {
+        collider = GetComponent<Collider>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             OnOpenShop?.Invoke();
+            Destroy(collider);
         }
     }
 }
