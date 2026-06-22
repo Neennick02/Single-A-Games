342150cf2725d9941c750b9304b709f294ce32ee
Yannick Garbe <Yannickgarbe02@gmail.com>
2026-06-22 11:43:05 +0200

    fix: update healthbar on floor change

--- Assets/Scripts/Health/PlayerHealthBar.cs
+++ Assets/Scripts/Health/PlayerHealthBar.cs
@@ -1,29 +1,38 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerHealthBar : MonoBehaviour
 {
     [SerializeField] private PlayerObject PlayerData;
     private Image _healthBar;
-    private float _prevHealth;
+    private float currentH;
     private void OnEnable()
     {
+        _healthBar = GetComponent<Image>();
+
         PlayerHealth.OnHealthChange += UpdateHealth;
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
     private void OnDisable()
     {
         PlayerHealth.OnHealthChange -= UpdateHealth;
-    }
-    private void Start()
-    {
-        _healthBar = GetComponent<Image>();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
     private void UpdateHealth(float current)
     {
         float fillAmount = current / PlayerData.MaxHealth;
+        currentH = current;
         _healthBar.fillAmount = fillAmount;
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "MainScene" && PlayerMotor.Instance != null)
+        {
+            UpdateHealth(PlayerMotor.Instance.GetComponent<PlayerHealth>().GetHealth());
+        }
     }
 }
