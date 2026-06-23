90f9bbe4b5605cbe2287b37f0737bf79466111dc
Nee_nnick <Yannickgarbe02@gmail.com>
2026-06-23 16:09:59 +0100

    add: shop textbubbles

--- Assets/Scripts/Tools/ReadWeiteHelper.cs
+++ Assets/Scripts/Tools/ReadWeiteHelper.cs
@@ -3,20 +3,5 @@ using UnityEngine;
 
 public class ReadWeiteHelper : MonoBehaviour
 {
-    [MenuItem("Tools/Enable ReadWrite On Selected Meshes")]
-    static void EnableRW()
-    {
-        foreach (Object obj in Selection.objects)
-        {
-            string path = AssetDatabase.GetAssetPath(obj);
-            ModelImporter importer = AssetImporter.GetAtPath(path) as ModelImporter;
-
-            if (importer != null)
-            {
-                importer.isReadable = true;
-                importer.SaveAndReimport();
-                Debug.Log("Enabled Read/Write on: " + obj.name);
-            }
-        }
-    }
+    
 }
