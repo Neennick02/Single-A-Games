3c3238da5255ba392b005680b3476e475b5132b5
raav <127980618+raav11@users.noreply.github.com>
2026-06-23 12:08:35 +0200

    screen shake and attack better and smoother

--- Assets/Scripts/Managers/CameraShakeManager.cs
+++ Assets/Scripts/Managers/CameraShakeManager.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 
 public class CameraShakeManager : MonoBehaviour
 {
-    public void CameraShake(CinemachineImpulseSource impulseSource, float intensity)
+    public void CameraShake(GameObject ob, float intensity)
     {
+
+        CinemachineImpulseSource impulseSource = ob.GetComponent<CinemachineImpulseSource>();
+
+        float X = Random.Range(-0.1f, 0.1f);
+        float Y = Random.Range(-0.1f, 0.1f);
+
+        impulseSource.DefaultVelocity = new Vector3(X, Y, 0f);
+
         impulseSource.GenerateImpulseWithForce(intensity * 10f);
     }
 }
