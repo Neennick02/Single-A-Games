4425ebd5d05a92b513c65b36ba4d58f7903dc7c7
Yannick Garbe <Yannickgarbe02@gmail.com>
2026-06-29 10:01:43 +0200

    add: start objective text

---
+++ Assets/Scripts/UI/ObjectivePopup.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class ObjectivePopup : MonoBehaviour
+{
+    public float duration = 3f;
+    public TextMeshProUGUI _textObject;
+
+    public Color StartC;
+    public Color EndC;
+
+    void OnEnable()
+    {
+        GameManager.OnShowObjectiveText += FadingMessage;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnShowObjectiveText -= FadingMessage;
+
+    }
+    public void FadingMessage(string message)
+    {
+        _textObject.text = message;
+        // StopAllCoroutines();
+        StartCoroutine(FadeInAndOut());
+    }
+
+    public void InstantMessage(string message, bool clear)
+    {
+        _textObject.color = Color.black;
+        _textObject.text = message;
+
+        if (clear)
+        {
+            _textObject.color = Color.clear;
+        }
+    }
+
+    IEnumerator FadeInAndOut()
+    {
+        float totalTimer = 0;
+        float endTimer = 0f;
+        //fade in
+        while (totalTimer < duration / 3)
+        {
+            totalTimer += Time.deltaTime;
+
+            _textObject.color = Color.Lerp(EndC, StartC, totalTimer / (duration / 3));
+
+            yield return null;
+        }
+
+        //have text visable
+        yield return new WaitForSeconds(duration / 3);
+        totalTimer += duration / 3;
+
+        //fade out
+        while (totalTimer < duration)
+        {
+            totalTimer += Time.deltaTime;
+            endTimer += Time.deltaTime;
+
+            _textObject.color = Color.Lerp(StartC, EndC, endTimer / (duration / 3));
+
+            yield return null;
+        }
+    }
+}
